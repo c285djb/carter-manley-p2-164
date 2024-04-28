9942945714d7613a3a55bd01eb7a6486e378aede
M C <manleyc1@gmatc.matc.edu>
2024-04-28 14:15:01 -0500

    Project 2 Added ThemeProvider

--- my-gatsby-project/src/components/ThemeProvider.js
+++ my-gatsby-project/src/components/ThemeProvider.js
@@ -1,1 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+import MyBox from './Box/Box'
+
+const theme = {
+  colors: {
+    primary: '#007bff',
+    text: '#333',
+  },
+}
+
+const MyApp = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <MyBox />
+    </ThemeProvider>
+  )
+}
 
